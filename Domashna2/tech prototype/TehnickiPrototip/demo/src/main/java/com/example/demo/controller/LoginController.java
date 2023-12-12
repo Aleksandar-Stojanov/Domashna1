4fffb6e25858a37b865c2f5a7c7ae1391d9c98a5
Aleksandar-Stojanov <aleksandar.stojanov@students.finki.ukim.mk>
2023-12-12 23:16:39 +0100

    checking the user from the database and corrected a weird mistake on the WineryController

--- Domashna2/tech prototype/TehnickiPrototip/demo/src/main/java/com/example/demo/controller/LoginController.java
+++ Domashna2/tech prototype/TehnickiPrototip/demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+import com.example.demo.repository.Users_winery_Repository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    //ovoj del za proverka na username pass ponatamu bi go sredil vo service del!
+    private final Users_winery_Repository usersWineryRepository;
+
+    public LoginController(Users_winery_Repository usersWineryRepository) {
+        this.usersWineryRepository = usersWineryRepository;
+    }
 
     // Display the login form
     @GetMapping("/login")
@@ -18,7 +25,7 @@ public class LoginController {
     public String processLogin(@RequestParam("username") String username,
                                @RequestParam("password") String password,
                                Model model) {
-        if (username.equals("admin") && password.equals("password")) {
+        if (usersWineryRepository.findByUsernameAndPassword(username,password)!=null) {
             return "redirect:/wine/home";
         } else {
             model.addAttribute("error", true);
